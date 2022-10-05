eebc7d647f0e1f9de759aae55983fbe39b92fe8b
Travis Henry <travis.henry2016@gmail.com>
2022-10-05 12:46:41 -0600

    Added images

---
+++ main.js
@@ -0,0 +1,40 @@
+
+var possibleCountries = {
+    spain:{
+        name:"Spain",
+        image:""
+    },
+    unitedStates:{
+        name:"United States",
+        image:""
+    },
+    france:{
+        name:"France",
+        image:""
+    }
+}
+
+
+
+
+
+
+function main(){
+
+
+
+
+
+}
+
+
+
+
+
+function setFlag(){
+    // var image = document.getElementById("picture");
+    // image.src = "\\resources\\flags\\ua.png";
+    // console.log("This ran")
+}
+
+setFlag();
